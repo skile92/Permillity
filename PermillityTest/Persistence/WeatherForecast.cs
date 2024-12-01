19244a1102bd8294688bd219dc86dc1328447456
skile92 <45519676+skile92@users.noreply.github.com>
2024-12-01 18:45:13 +0100

    Solution cleanup

--- PermillityTest/Persistence/WeatherForecast.cs
+++
@@ -1,9 +0,0 @@
-namespace PermillityTest.Persistence;
-
-public class WeatherForecast
-{
-    public int Id { get; set; }
-    public DateOnly Date { get; set; }
-    public int TemperatureC { get; set; }
-    public string? Summary { get; set; }
-}
--- PermillityTest/Services/CreateWeatherForecast.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace PermillityTest.Services;
-
-public class CreateWeatherForecast
-{
-    public DateOnly Date { get; set; }
-    public int TemperatureC { get; set; }
-    public string? Summary { get; set; }
-}
